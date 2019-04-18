eb1d21b96f851a112668e9831cd1949c990f7b46
Markus Sanders <markus.sanderst@gmail.com>
2019-04-18 23:12:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language) 
-  puts `Hello, my name is #{name}
+  puts `Hello, my name is #{name} and I am learning to program in #{language}`
+end
